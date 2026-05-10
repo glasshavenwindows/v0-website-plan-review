e7b884957111081aa6a1d6b7e1600e2c6a415338
glasshavenwindows <glasshavenwindows@gmail.com>
2026-05-09 23:48:25 -0600

    Update layout.tsx
    Added Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script' // This is required for the Pixel to work
 import './globals.css'
 import { Header } from '@/components/header'
 import { Footer } from '@/components/footer'
@@ -58,7 +59,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-background">
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2143457473120173');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body className="font-sans antialiased min-h-screen flex flex-col">
+        {/* Fallback for users with JavaScript disabled */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2143457473120173&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
