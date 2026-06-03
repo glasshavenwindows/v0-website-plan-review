3c319fa79c1c4631d551a3404d6930b5f0adc9fe
glasshavenwindows <glasshavenwindows@gmail.com>
2026-06-02 23:21:35 -0600

    Update chat-widget.tsx

--- components/chat-widget.tsx
+++ components/chat-widget.tsx
@@ -1,23 +1,5 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { useEffect } from 'react'
-
 export function ChatWidget() {
-  const pathname = usePathname()
-
-  useEffect(() => {
-    const toggle = () => {
-      const widget = document.querySelector('chat-widget') as HTMLElement | null
-      if (widget) {
-        widget.style.display = pathname === '/contact' ? 'none' : ''
-      }
-    }
-
-    toggle()
-    const timer = setTimeout(toggle, 800)
-    return () => clearTimeout(timer)
-  }, [pathname])
-
   return null
 }
