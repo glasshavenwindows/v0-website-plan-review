4df1b349c01107fd90972e8a0c555e0374d09304
glasshavenwindows <panelyft@gmail.com>
2026-06-25 15:53:21 -0600

    Update Meta Pixel ID to Glass Haven V2 Dataset (1693183235165089)

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,7 +89,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2143457473120173');
+            fbq('init', '1693183235165089');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -117,7 +117,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=2143457473120173&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1693183235165089&ev=PageView&noscript=1"
           />
         </noscript>
 
